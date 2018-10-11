bf6347eaa5ce25d35254f9e4ea6aefb6da2d260b
unknown <2120343354@qq.com>
2018-10-11 15:58:43 +0800

    models_and_databases --20181011

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     """一个博客的模型"""
-    auther = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
