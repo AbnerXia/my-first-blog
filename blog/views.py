26b6d6bf3ccaec202b209626fccaa28cee15990a
unknown <2120343354@qq.com>
2018-10-11 19:56:14 +0800

    one_post_page_and_more_css --20181011

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 
 def post_list(request):
-    """主页路由的试图函数"""
+    """主页路由的视图函数"""
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    """单个博客文章的路由的视图函数"""
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
